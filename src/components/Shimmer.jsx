1454af01c5e829c6657fa70d7e6a5efe8fc9aa3e
Lakshya Vyas <lakshyavyas987@gmail.com>
2023-09-06 21:12:35 +0530

    Shimmer Done

--- src/components/Shimmer.jsx
+++ src/components/Shimmer.jsx
@@ -1,8 +1,21 @@
-
 const Shimmer = () => {
   return (
-    <div>Loading.........</div>
-  )
-}
-
-export default Shimmer
+    <>
+      <div className="flex mt-3 justify-end mr-12">
+        <input type="text" placeholder="Search..." />
+        <button>Search</button>
+      </div>
+      <div className="flex flex-row justify-evenly flex-wrap">
+        {Array(8)
+          .fill("")
+          .map((e, index) => (
+            <div
+              key={index}
+              className="text-center mt-5 w-80 m-5 h-96 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+            ></div>
+          ))}
+      </div>
+    </>
+  );
+};
+export default Shimmer;
